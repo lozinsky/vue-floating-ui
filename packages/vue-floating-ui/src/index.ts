eb79640a2c36e35a1cc2929f064906ab5a2fb8e2
Pavel Lozinsky <pavel@lozinsky.dev>
2022-11-07 16:12:59 +0300

    feat: introduce `vue-floating-ui` package

---
+++ packages/vue-floating-ui/src/index.ts
@@ -0,0 +1,3 @@
+export { type UseFloatingOptions, type UseFloatingReturn, useFloating } from './composables/useFloating';
+export { type ArrowOptions, arrow } from './middleware/arrow';
+export * from '@floating-ui/dom';
---
+++ packages/vue-floating-ui/src/utils/unwrapElement.ts
@@ -0,0 +1,10 @@
+import type { ComponentPublicInstance } from 'vue-demi';
+
+import type { MaybeElement } from '../types';
+
+export function unwrapElement<T>(element: MaybeElement<T>) {
+  return ((element as Exclude<MaybeElement<T>, T>)?.$el ?? element) as Exclude<
+    MaybeElement<T>,
+    ComponentPublicInstance
+  >;
+}
